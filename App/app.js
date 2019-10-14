45834c55178b2a1c6b0ca85c6329395f5c2a9d06
Leidy Martinez <leidymartinezsanchez2000@gmail.com>
2019-10-14 15:09:12 -0500

    Nuevo

---
+++ App/app.js
@@ -0,0 +1,10 @@
+var bienvenida = document.getElementById("bienvenida");
+var text = document.getElementById("Texto");
+document.getElementById("Lista").classList.remove("fila1");
+function funcion(){
+    alert("Hola a todos");
+bienvenida.innerHTML = "Hola a todos";
+text.hidden = text;
+
+
+}
